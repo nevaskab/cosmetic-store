bd08d8b3af2d9c3b5c71605780253df6fb1d53c2
Andressa Martins <andressa.devsystem@gmail.com>
2025-07-24 12:42:49 -0300

    [update]: swiper config

--- script.js
+++ script.js
@@ -59,4 +59,22 @@ document.addEventListener('DOMContentLoaded', () =>{
         setButtonStyle(buttons[0]);
         filterProducts('todos')
     }
+
+    var swiper = new Swiper('.coverflow-swiper', {
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        coverflowEffect: {
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
 })
